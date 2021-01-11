6c3f5c9dda84e287281db2e4b86d52896a98d39c
c3-villaurbia <Carlo2-Villarubia@live.uwe.ac.uk>
2021-01-11 11:23:07 -0500

    carlo added to syscall

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -14,11 +14,14 @@ void halt(void);
 pid_t exec(const char *cmd_line);
 int open(const char *file);
 bool create(const char * file,unsigned initial_size);
-int write(int fd, const void *buffer, unsigned size);
+bool remove (const char *file);
+int read (int fd, void *buffer, unsigned size);// carlo - added 11/01/21
+int write(int fd, const void *buffer, unsigned size);// carlo - added 11/01/21
 void close(int fd);
 void close_file(int fd);
 void exit(int status);
 off_t fileSize(int fd);
+unsigned tell (int fd);// carlo - added 11/01/21
 void seek(int fd, unsigned position);
 int set_file(struct file *fn);
 struct file* get_file (int fd);
@@ -41,13 +44,28 @@ syscall_handler (struct intr_frame *f UNUSED)
   	case SYS_HALT:{
   		halt();
   		}break;
-		
+
+	case SYS_EXEC:{// carlo - added 11/01/21
+			char* cmd_line;
+			int return_code = exec((const char*)cmd_line);
+
+			f->eax = (uint32_t) return_code;
+			
+		}break;
+
   	case SYS_OPEN:{
   		char *filename = f->esp + 1;
 		printf("FILE: %c", *filename);
 		f->eax = open(filename);
   		}break;
-			
+		
+  	case SYS_READ:{// carlo - added 11/01/21
+		int *filename = f->esp + 1;
+		char *buffer = f->esp + 2;
+		unsigned *size = f->esp + 3;
+		f->eax = read((int)filename, (void *)buffer,(unsigned)size);
+		}break;
+
 	case SYS_WRITE:{
 		int *filename = f->esp + 1;
 		char *buffer = f->esp + 2;
@@ -66,11 +84,20 @@ syscall_handler (struct intr_frame *f UNUSED)
  		f->eax = create(file,initial_size);
   		}break;
 
+  	case SYS_REMOVE:{// carlo - added 11/01/21
+  		char* file_name = f->eax+1;
+  		f->eax = remove(file_name);
+	  	}break;
+  	
   	case SYS_FILESIZE:{
-		int fdFileSize = *((int*)f->esp + 1);  		
+		int fdFileSize = *((int*)f->esp + 1);		
   		f->eax = fileSize(fdFileSize);
   		}break;
   		
+  	case SYS_TELL:{// carlo - added 11/01/21
+  		f->eax = tell((int*)f->esp+1);
+  		}break;
+
   	case SYS_SEEK:{
   	    int fdSeek = *((int*)f->esp + 1);
   		unsigned position = *((unsigned*)f->esp + 2);
@@ -87,6 +114,44 @@ syscall_handler (struct intr_frame *f UNUSED)
 /*21/12/2020 - Ethan Donovan*/
 void halt(void){
 	shutdown_power_off();
+}
+
+/*runs program and returns pid*/
+pid_t exec(const char *cmd_line){// carlo - added 11/01/21
+	
+	pid_t id = process_execute(cmd_line);
+	return id;
+}
+
+//read size of bytes
+int read (int fd, void *buffer, unsigned size){// carlo - added 11/01/21
+	int s=0;//size of byte
+
+	//read from keyboard using input_getc()
+	if(fd == OUTPUT){
+		while(s < size){
+			*((char*)buffer+s)=input_getc();
+			s++;
+		}
+		return s;
+	}
+	//Get file from file struct
+	struct file *file_to_read = get_file(fd);
+	//if no there is no file
+	if(file_to_read == NULL){
+		return -1;
+	}
+	//return file size
+	s = file_read(file_to_read,buffer,size);
+	return s;
+}
+
+bool remove (const char *file){// carlo - added 11/01/21
+	if(file == NULL){
+		return -1;
+	}
+	bool succes = filesys_remove(file);
+	return succes;
 }
 
 // 21/12/2020 - Benjamin ELl-Jones 
@@ -110,7 +175,17 @@ bool create(const char * file,unsigned initial_size){/*Create file*/
 	return isCreated;
  }
 
-//remove
+unsigned tell (int fd){// carlo - added 11/01/21
+	struct file *tell_next_byte = get_file(fd);
+	
+	if (tell_next_byte == NULL){
+		return -1;
+
+	}
+	unsigned pos_of_byte = file_tell(tell_next_byte);
+	return pos_of_byte;
+
+}
 
 
 //18/12/2020 - Ethan Donovan
